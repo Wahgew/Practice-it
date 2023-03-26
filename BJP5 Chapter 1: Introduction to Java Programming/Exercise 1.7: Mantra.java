c0b196587a7dfcc2570dc5f018b519d34559af75
Peter Madin <122696978+Wahgew@users.noreply.github.com>
2023-03-26 14:30:45 -0700

    Create Exercise 1.7: Mantra.java

---
+++ BJP5 Chapter 1: Introduction to Java Programming/Exercise 1.7: Mantra.java
@@ -0,0 +1,19 @@
+/* Write a complete Java program in a class named Mantra that produces the following output. Remove its redundancy by adding a method. 
+
+There's one thing every coder must understand:
+The System.out.println command.
+
+There's one thing every coder must understand:
+The System.out.println command.
+*/
+
+public class Mantra {
+    public static void main(String[] theArgs) {
+        copy();
+        copy();
+    }
+    public static void copy() {
+        System.out.println("There's one thing every coder must understand:");
+        System.out.println("The System.out.println command.\n");
+    }
+}
