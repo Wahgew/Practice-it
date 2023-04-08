898156f2a05e73dc14bb299bb414fe9d585f0b2e
Peter Madin <122696978+Wahgew@users.noreply.github.com>
2023-04-08 13:17:55 -0700

    Create Exercise 2.2: loopSquares.java

---
+++ BJP5 Chapter 2: Primitive Data and Definite Loops/Exercise 2.2: loopSquares.java
@@ -0,0 +1,11 @@
+/* Write a for loop that produces the following output: 
+
+1 4 9 16 25 36 49 64 81 100 
+
+For added challenge, try to modify your code so that it does not need to use the * multiplication operator. 
+(It can be done! Hint: Look at the differences between adjacent numbers.)
+*/
+
+for (int num = 1; num <= 10; num++) {
+    System.out.print(num * num + " ");
+}
