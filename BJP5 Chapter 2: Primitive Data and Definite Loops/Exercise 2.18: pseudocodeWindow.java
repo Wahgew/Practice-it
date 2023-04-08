1b7d0ce052fe670d09fe444a8a7ef3a4dff46b79
Peter Madin <122696978+Wahgew@users.noreply.github.com>
2023-04-08 13:44:31 -0700

    Create Exercise 2.18: pseudocodeWindow.java

---
+++ BJP5 Chapter 2: Primitive Data and Definite Loops/Exercise 2.18: pseudocodeWindow.java
@@ -0,0 +1,31 @@
+/* Write a pseudocode algorithm that will produce the following figure as output: 
+
++===+===+
+|   |   |
+|   |   |
+|   |   |
++===+===+
+|   |   |
+|   |   |
+|   |   |
++===+===+
+
+(Since this is just pseudo-code, Practice-It is not able to verify that your solution is "correct". 
+But you can submit an answer anyway to show that you completed this exercise. Any non-trivial answer will be accepted.)
+
+As said above you could pretty much write anything as long as your write enough lines of text you'll pass.
+
+Create a method called frame
+ in this method print(+===+===+)
+
+Create a method called window
+ in this method print(|   |   |) three times
+
+call methods in the order of
+
+frame
+window
+frame
+window
+frame
+*/
