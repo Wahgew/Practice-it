119fb4ca337193c8c51a3c0b72a8b7821e825c60
Peter Madin <122696978+Wahgew@users.noreply.github.com>
2023-04-18 13:41:18 -0700

    Create Exercise 3.14: cylinderSurfaceArea.java

---
+++ BJP5 Chapter 3: Parameters and Objects/Exercise 3.14: cylinderSurfaceArea.java
@@ -0,0 +1,7 @@
+/* Write a method called cylinderSurfaceArea that accepts a radius and height (both real numbers) as parameters and returns the surface area of a cylinder with those dimensions. 
+For example, the call cylinderSurfaceArea(3.0, 4.5) should return 141.3716694115407. The formula for the surface area of a cylinder with radius r and height h is the following:
+*/
+
+public static double cylinderSurfaceArea(double radius, double height) {
+    return (2 * Math.PI * (radius * radius)) + (2 * Math.PI * radius * height);
+}
