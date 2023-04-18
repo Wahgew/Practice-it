004b1eb5148f25e1e0840139073266a962e86f7c
Peter Madin <122696978+Wahgew@users.noreply.github.com>
2023-04-18 13:33:00 -0700

    Update Exercise 3.9: lastDigit.java

--- BJP5 Chapter 3: Parameters and Objects/Exercise 3.9: lastDigit.java
+++ BJP5 Chapter 3: Parameters and Objects/Exercise 3.9: lastDigit.java
@@ -2,11 +2,11 @@
 For example, lastDigit(3572) should return 2. It should work for negative numbers as well. For example, lastDigit(-947) should return 7.
 
 Call	          Value Returned
-lastDigit(3572)	       2
-lastDigit(-947)	       7
+lastDigit(3572)         2
+lastDigit(-947)         7
 lastDigit(6)	         6
 lastDigit(35)        	 5
-lastDigit(123456)	     6
+lastDigit(123456)       6
 
 (Hint: This is a short method. You may not use a String to solve this problem.)
 */
